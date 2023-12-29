fe982f0d7cf1f58719c4d22fe996fe432803e2dd
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-12-30 00:09:33 +0100

    Updating initial commit

--- src/main/java/org/example/WorkingWithStack.java
+++ src/main/java/org/example/WorkingWithStack.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class WorkingWithStack {
     public static void main(String[] args) {
         Stack<String> stack = new Stack<String>();
-//        System.out.println(stack.empty());
+        System.out.println(stack.empty());
         stack.push("James");
 
         stack.push("Desmond");
@@ -15,14 +15,23 @@ public class WorkingWithStack {
         stack.push("Collins");
 
         stack.push("Queen");
+
         stack.push("Samuel");
+
         stack.push("David");
+
         stack.push("Paul");
+
         System.out.println(stack.empty());
+
         System.out.println(stack.search("John"));
+
         System.out.println(stack.peek());
+
         System.out.println(stack.pop());
+
         System.out.println(stack.pop());
+        
         System.out.println(stack);
 
     }
